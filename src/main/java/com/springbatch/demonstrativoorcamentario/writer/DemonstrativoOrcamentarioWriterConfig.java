e69328201095b0560e0b0f335829fd2c400e097d
Edson Araujo <edson.caraujo@hotmail.com>
2020-08-30 22:23:28 -0300

    Commit da alteração para gerar o arquivo estruturado

--- src/main/java/com/springbatch/demonstrativoorcamentario/writer/DemonstrativoOrcamentarioWriterConfig.java
+++ src/main/java/com/springbatch/demonstrativoorcamentario/writer/DemonstrativoOrcamentarioWriterConfig.java
@@ -3,28 +3,64 @@ package com.springbatch.demonstrativoorcamentario.writer;
 import java.text.NumberFormat;
 import java.text.SimpleDateFormat;
 
-import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.item.file.FlatFileItemWriter;
+import org.springframework.batch.item.file.builder.FlatFileItemWriterBuilder;
+import org.springframework.batch.item.file.transform.LineAggregator;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
 
 import com.springbatch.demonstrativoorcamentario.dominio.GrupoLancamento;
 import com.springbatch.demonstrativoorcamentario.dominio.Lancamento;
 
 @Configuration
 public class DemonstrativoOrcamentarioWriterConfig {
+//	@Bean
+//	public ItemWriter<GrupoLancamento> demonstrativoOrcamentarioWriter() {
+//		return itens -> {
+//			System.out.println("---- Demonstrativo orçamentário ----");
+//			for (GrupoLancamento grupo : itens) {
+//				System.out.println(String.format("[%d] %s - %s", grupo.getCodigoNaturezaDespesa(),
+//						grupo.getDescricaoNaturezaDespesa(),
+//						NumberFormat.getCurrencyInstance().format(grupo.getTotal())));
+//				for (Lancamento lancamento : grupo.getLancamentos()) {
+//					System.out.println(String.format("\t [%s] %s - %s", new SimpleDateFormat("dd/MM/yyyy").format(lancamento.getData()), lancamento.getDescricao(),
+//							NumberFormat.getCurrencyInstance().format(lancamento.getValor())));
+//				}
+//			}
+//		};
+//	}
+	@StepScope
 	@Bean
-	public ItemWriter<GrupoLancamento> demonstrativoOrcamentarioWriter() {
-		return itens -> {
-			System.out.println("---- Demonstrativo orçamentário ----");
-			for (GrupoLancamento grupo : itens) {
-				System.out.println(String.format("[%d] %s - %s", grupo.getCodigoNaturezaDespesa(),
-						grupo.getDescricaoNaturezaDespesa(),
-						NumberFormat.getCurrencyInstance().format(grupo.getTotal())));
-				for (Lancamento lancamento : grupo.getLancamentos()) {
-					System.out.println(String.format("\t [%s] %s - %s", new SimpleDateFormat("dd/MM/yyyy").format(lancamento.getData()), lancamento.getDescricao(),
-							NumberFormat.getCurrencyInstance().format(lancamento.getValor())));
+	public FlatFileItemWriter<GrupoLancamento> demonstrativoOrcamentarioWriter(
+			@Value("#{jobParameters['demonstrativoOrcamentario']}") Resource demonstrativoOrcamentario){
+		return new FlatFileItemWriterBuilder<GrupoLancamento>()
+				.name("demonstrativoOrcamentarioWriter")
+				.resource(demonstrativoOrcamentario)
+				.lineAggregator(lineAggregator())
+				.build();
+	}
+
+	private LineAggregator<GrupoLancamento> lineAggregator() {
+		// TODO Auto-generated method stub
+		return new LineAggregator<GrupoLancamento>() {
+
+			@Override
+			public String aggregate(GrupoLancamento grupoLancamento) {
+				String formatGrupoLancamento = String.format("[%d] %s - %s\n", grupoLancamento.getCodigoNaturezaDespesa(),
+						grupoLancamento.getDescricaoNaturezaDespesa(),
+						NumberFormat.getCurrencyInstance().format(grupoLancamento.getTotal()));
+				StringBuilder strBuilder = new StringBuilder();
+				for(Lancamento lancamento : grupoLancamento.getLancamentos()) {
+					strBuilder.append(String.format("\t [%s] %s - %s\n", new SimpleDateFormat("dd/MM/yyyy").format(lancamento.getData()), lancamento.getDescricao(),
+							NumberFormat.getCurrencyInstance().format(lancamento.getValor())));					
 				}
+				String formatLancamento = strBuilder.toString();
+				return formatGrupoLancamento + formatLancamento;
 			}
 		};
 	}
+		
 }
